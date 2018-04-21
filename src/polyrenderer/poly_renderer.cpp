bcb2ba6f29937bda2c1a33d87c4f9626037412b8
Magnus Norddahl <dpjudas@users.noreply.github.com>
2018-04-21 15:19:28 +0200

    - Simplify the portal code in softpoly somewhat
    (cherry picked from commit 16a8d71caf669e7f45b2da85d5904eca7d1d2a34)
    
    # Conflicts:
    #	src/polyrenderer/poly_renderer.cpp

--- src/polyrenderer/poly_renderer.cpp
+++ src/polyrenderer/poly_renderer.cpp
@@ -161,17 +161,14 @@ void PolyRenderer::RenderActorView(AActor *actor, bool dontmaplines)
 
 	ClearBuffers();
 	SetSceneViewport();
-	SetupPerspectiveMatrix();
 
-	PolyPortalViewpoint mainViewpoint;
-	mainViewpoint.WorldToView = WorldToView;
-	mainViewpoint.WorldToClip = WorldToClip;
+	PolyPortalViewpoint mainViewpoint = SetupPerspectiveMatrix();
 	mainViewpoint.StencilValue = GetNextStencilValue();
 	mainViewpoint.PortalPlane = PolyClipPlane(0.0f, 0.0f, 0.0f, 1.0f);
+	Scene.CurrentViewpoint = &mainViewpoint;
 	Scene.Render(&mainViewpoint);
-	Skydome.Render(Threads.MainThread(), WorldToView, WorldToClip);
-	Scene.RenderTranslucent(&mainViewpoint);
 	PlayerSprites.Render(Threads.MainThread());
+	Scene.CurrentViewpoint = nullptr;
 
 	if (Viewpoint.camera)
 		Viewpoint.camera->renderflags = savedflags;
@@ -216,7 +213,7 @@ void PolyRenderer::SetSceneViewport()
 	}
 }
 
-void PolyRenderer::SetupPerspectiveMatrix()
+PolyPortalViewpoint PolyRenderer::SetupPerspectiveMatrix()
 {
 	static bool bDidSetup = false;
 
@@ -236,9 +233,12 @@ void PolyRenderer::SetupPerspectiveMatrix()
 
 	float ratio = Viewwindow.WidescreenRatio;
 	float fovratio = (Viewwindow.WidescreenRatio >= 1.3f) ? 1.333333f : ratio;
+
 	float fovy = (float)(2 * DAngle::ToDegrees(atan(tan(Viewpoint.FieldOfView.Radians() / 2) / fovratio)).Degrees);
 
-	WorldToView =
+	PolyPortalViewpoint portalViewpoint;
+
+	portalViewpoint.WorldToView =
 		Mat4f::Rotate((float)Viewpoint.Angles.Roll.Radians(), 0.0f, 0.0f, 1.0f) *
 		Mat4f::Rotate(adjustedPitch, 1.0f, 0.0f, 0.0f) *
 		Mat4f::Rotate(adjustedViewAngle, 0.0f, -1.0f, 0.0f) *
@@ -246,7 +246,9 @@ void PolyRenderer::SetupPerspectiveMatrix()
 		Mat4f::SwapYZ() *
 		Mat4f::Translate((float)-Viewpoint.Pos.X, (float)-Viewpoint.Pos.Y, (float)-Viewpoint.Pos.Z);
 
-	WorldToClip = Mat4f::Perspective(fovy, ratio, 5.0f, 65535.0f, Handedness::Right, ClipZRange::NegativePositiveW) * WorldToView;
+	portalViewpoint.WorldToClip = Mat4f::Perspective(fovy, ratio, 5.0f, 65535.0f, Handedness::Right, ClipZRange::NegativePositiveW) * portalViewpoint.WorldToView;
+
+	return portalViewpoint;
 }
 
 cycle_t PolyCullCycles, PolyOpaqueCycles, PolyMaskedCycles, PolyDrawerWaitCycles;
