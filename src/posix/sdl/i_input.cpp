5f850067b3d89eab6d047172adf9a30d701e4566
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-05-06 14:37:04 +0300

    - SDL: clear button state when switching from/to GUI input
    https://forum.zdoom.org/viewtopic.php?t=60451
    (cherry picked from commit 7fcefa2ed11928a5c8c94b7b79f33613b121b873)
    
    # Conflicts:
    #	src/posix/sdl/i_input.cpp

--- src/posix/sdl/i_input.cpp
+++ src/posix/sdl/i_input.cpp
@@ -46,6 +46,7 @@
 #include "c_console.h"
 #include "c_cvars.h"
 #include "i_system.h"
+#include "c_dispatch.h"
 #include "dikeys.h"
 #include "templates.h"
 #include "s_sound.h"
@@ -199,6 +200,7 @@ static void I_CheckGUICapture ()
 		{
 			memset (DownState, 0, sizeof(DownState));
 		}
+		ResetButtonStates();
 	}
 }
 
