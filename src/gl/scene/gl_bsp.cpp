ZDoom/gzdoom

- some Transfer_Heights related optimizations.
* only call hw_CheckViewArea if the result is not known yet.
* check the map up front if it even contains heightsecs. This allows to shortcut the above check entirely for maps without sector transfers and will allow further optimizations.

src/gl/scene/gl_bsp.cpp
@@ -140,7 +140,7 @@ void GLSceneDrawer::AddLine (seg_t *seg, bool portalclip)
 		else
 		{
 			// clipping checks are only needed when the backsector is not the same as the front sector
-			in_area = hw_CheckViewArea(in_area, seg->v1, seg->v2, seg->frontsector, seg->backsector);
+			if (in_area == area_default) in_area = hw_CheckViewArea(seg->v1, seg->v2, seg->frontsector, seg->backsector);
 
 			backsector = hw_FakeFlat(seg->backsector, &bs, in_area, true);
 

ZDoom/gzdoom-gles

(cherry picked from commit 3c49804c6c17aa7c5bc5d895e64fc37dcc1c4678)
# Conflicts:
#	src/gl/scene/gl_bsp.cpp
#	src/gl/scene/gl_fakeflat.cpp
#	src/hwrenderer/scene/hw_fakeflat.h

src/gl/scene/gl_bsp.cpp
@@ -147,7 +147,7 @@ void GLSceneDrawer::AddLine (seg_t *seg, bool portalclip)
 		else
 		{
 			// clipping checks are only needed when the backsector is not the same as the front sector
-			CheckViewArea(seg->v1, seg->v2, seg->frontsector, seg->backsector);
+			if (in_area == area_default) in_area = CheckViewArea(seg->v1, seg->v2, seg->frontsector, seg->backsector);
 
 			backsector = gl_FakeFlat(seg->backsector, &bs, in_area, true);
 