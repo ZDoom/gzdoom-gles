4902143d7b211710d32e486b761b6c02904e27ee
David Carlier <devnexen@gmail.com>
2018-07-05 21:10:07 +0100

    foo being allocated with metadata based allocator needs to be freed similarly.
    (cherry picked from commit 2a59327aebcdea245c96dddcb255ed15f8c38c05)

--- src/textures/rawpagetexture.cpp
+++ src/textures/rawpagetexture.cpp
@@ -94,7 +94,7 @@ static bool CheckIfRaw(FileReader & data)
 			}
 			else if (ofs >= 64000-1)	// Need one byte for an empty column
 			{
-				free (foo);
+				M_Free (foo);
 				return true;
 			}
 			else
@@ -105,14 +105,14 @@ static bool CheckIfRaw(FileReader & data)
 				{
 					if (foo2[ofs] == 255)
 					{
-						free (foo);
+						M_Free (foo);
 						return true;
 					}
 					ofs += foo2[ofs+1] + 4;
 				}
 				if (ofs >= 64000)
 				{
-					free (foo);
+					M_Free (foo);
 					return true;
 				}
 			}
