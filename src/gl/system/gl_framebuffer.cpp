6f322940fef889bb4023847645eb46a82c0466b0
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-07-22 11:40:12 +0200

    - don't let the video scale let the screen end up with a client size less than 320x200, which may cause undefined behavior and trigger asserts in debug builds.
    # Conflicts:
    #	src/gl/system/gl_framebuffer.cpp
    #	src/v_video.cpp

--- src/gl/system/gl_framebuffer.cpp
+++ src/gl/system/gl_framebuffer.cpp
@@ -179,6 +179,8 @@ void OpenGLFrameBuffer::Update()
 	int initialHeight = IsFullscreen() ? VideoHeight : GetClientHeight();
 	int clientWidth = ViewportScaledWidth(initialWidth, initialHeight);
 	int clientHeight = ViewportScaledHeight(initialWidth, initialHeight);
+	if (clientWidth < 320) clientWidth = 320;
+	if (clientHeight < 200) clientHeight = 200;
 	if (clientWidth > 0 && clientHeight > 0 && (Width != clientWidth || Height != clientHeight))
 	{
 		// Do not call Resize here because it's only for software canvases
