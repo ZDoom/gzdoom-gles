e84b360cb5cf9ebcefe920a208b9f084da3a9b55
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-08-18 15:20:38 +0300

    - prohibit assignment of dynamic arrays
    https://forum.zdoom.org/viewtopic.php?t=61682

--- src/scripting/backend/codegen.cpp
+++ src/scripting/backend/codegen.cpp
@@ -2488,6 +2488,12 @@ FxExpression *FxAssign::Resolve(FCompileContext &ctx)
 			delete this;
 			return nullptr;
 		}
+		else if (Base->IsDynamicArray())
+		{
+			ScriptPosition.Message(MSG_ERROR, "Cannot assign dynamic arrays, use Copy() or Move() function instead");
+			delete this;
+			return nullptr;
+		}
 		if (!Base->IsVector() && Base->ValueType->isStruct())
 		{
 			ScriptPosition.Message(MSG_ERROR, "Struct assignment not implemented yet");
