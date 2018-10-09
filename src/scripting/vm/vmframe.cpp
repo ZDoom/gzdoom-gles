03689cf00488424f508ace8712545248127d900e
Magnus Norddahl <dpjudas@users.noreply.github.com>
2018-10-09 02:08:15 +0200

    - modify the VM calling convention so that the callee sets up its own VM frame

--- src/scripting/vm/vmframe.cpp
+++ src/scripting/vm/vmframe.cpp
@@ -448,8 +448,9 @@ VMFrame *VMFrameStack::PopFrame()
 
 int VMCall(VMFunction *func, VMValue *params, int numparams, VMReturn *results, int numresults/*, VMException **trap*/)
 {
-	bool allocated = false;
+#if 0
 	try
+#endif
 	{	
 		if (func->VarFlags & VARF_Native)
 		{
@@ -474,12 +475,7 @@ int VMCall(VMFunction *func, VMValue *params, int numparams, VMReturn *results, 
 			{
 				VMCycles[0].Clock();
 				VMCalls[0]++;
-				auto &stack = GlobalVMStack;
-				stack.AllocFrame(static_cast<VMScriptFunction *>(func));
-				allocated = true;
-				VMFillParams(params, stack.TopFrame(), numparams);
-				int numret = VMExec(&stack, code, results, numresults);
-				stack.PopFrame();
+				int numret = VMExec(static_cast<VMScriptFunction *>(func), params, numparams, results, numresults);
 				VMCycles[0].Unclock();
 				return numret;
 			}
@@ -488,10 +484,6 @@ int VMCall(VMFunction *func, VMValue *params, int numparams, VMReturn *results, 
 #if 0
 	catch (VMException *exception)
 	{
-		if (allocated)
-		{
-			PopFrame();
-		}
 		if (trap != NULL)
 		{
 			*trap = exception;
@@ -500,14 +492,6 @@ int VMCall(VMFunction *func, VMValue *params, int numparams, VMReturn *results, 
 		throw;
 	}
 #endif
-	catch (...)
-	{
-		if (allocated)
-		{
-			GlobalVMStack.PopFrame();
-		}
-		throw;
-	}
 }
 
 // Exception stuff for the VM is intentionally placed there, because having this in vmexec.cpp would subject it to inlining
