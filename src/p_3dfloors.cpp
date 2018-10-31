a40d717006a546c4b4662c2c7addc1f44f7f90ab
ZZYZX <zzyzx@virtual>
2018-10-31 21:56:01 +0200

    Fixed sector floor/ceiling actions not triggering in P_XYMovement

--- src/p_3dfloors.cpp
+++ src/p_3dfloors.cpp
@@ -349,7 +349,7 @@ void P_PlayerOnSpecial3DFloor(player_t* player)
 // Checks whether the player's feet touch a solid 3D floor in the sector
 //
 //==========================================================================
-bool P_CheckFor3DFloorHit(AActor * mo, double z)
+bool P_CheckFor3DFloorHit(AActor * mo, double z, bool trigger)
 {
 	if ((mo->player && (mo->player->cheats & CF_PREDICTING))) return false;
 
@@ -361,7 +361,8 @@ bool P_CheckFor3DFloorHit(AActor * mo, double z)
 		{
 			if (fabs(z - rover->top.plane->ZatPoint(mo)) < EQUAL_EPSILON) 
 			{
-				rover->model->TriggerSectorActions (mo, SECSPAC_HitFloor);
+				mo->BlockingFloor = rover->model;
+				if (trigger) rover->model->TriggerSectorActions (mo, SECSPAC_HitFloor);
 				return true;
 			}
 		}
@@ -375,7 +376,7 @@ bool P_CheckFor3DFloorHit(AActor * mo, double z)
 // Checks whether the player's head touches a solid 3D floor in the sector
 //
 //==========================================================================
-bool P_CheckFor3DCeilingHit(AActor * mo, double z)
+bool P_CheckFor3DCeilingHit(AActor * mo, double z, bool trigger)
 {
 	if ((mo->player && (mo->player->cheats & CF_PREDICTING))) return false;
 
@@ -387,7 +388,8 @@ bool P_CheckFor3DCeilingHit(AActor * mo, double z)
 		{
 			if(fabs(z - rover->bottom.plane->ZatPoint(mo)) < EQUAL_EPSILON)
 			{
-				rover->model->TriggerSectorActions (mo, SECSPAC_HitCeiling);
+				mo->BlockingCeiling = rover->model;
+				if (trigger) rover->model->TriggerSectorActions (mo, SECSPAC_HitCeiling);
 				return true;
 			}
 		}
