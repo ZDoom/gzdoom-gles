dc29eee077e116b0065dab40e79fddf092de036b
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-12-03 09:14:11 +0100

    - fixed compile warning

--- src/p_map.cpp
+++ src/p_map.cpp
@@ -3579,7 +3579,7 @@ bool FSlide::BounceWall(AActor *mo)
 	}
 	line = bestslideline;
 
-	if (line->special == Line_Horizon || (mo->BounceFlags & BOUNCE_NotOnSky) && line->hitSkyWall(mo))
+	if (line->special == Line_Horizon || ((mo->BounceFlags & BOUNCE_NotOnSky) && line->hitSkyWall(mo)))
 	{
 		mo->SeeSound = mo->BounceSound = 0;	// it might make a sound otherwise
 		mo->Destroy();
