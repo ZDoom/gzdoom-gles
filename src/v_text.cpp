651dce449a9b59f7b49e952b05ee615a167d357a
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-12-09 13:00:36 +0200

    - removed erroneous assertion
    https://forum.zdoom.org/viewtopic.php?t=62815

--- src/v_text.cpp
+++ src/v_text.cpp
@@ -223,8 +223,6 @@ void DCanvas::DrawTextCommon(FFont *font, int normalcolor, double x, double y, c
 	int			kerning;
 	FTexture *pic;
 
-	assert(string[0] != '$');
-
 	if (parms.celly == 0) parms.celly = font->GetHeight() + 1;
 	parms.celly *= parms.scaley;
 
