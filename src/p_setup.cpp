a82d60bb71b774ae532cd33bd1d8b8ba276afebe
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-12-27 08:28:09 +0100

    - fixed 3D floor initialization for actor spawning.
    Since actors are being spawned before the renderer gets set up this needs to fully initialize the list before spawning the actors, then take it down again for creating the vertex buffer and then recreate it.

--- src/p_setup.cpp
+++ src/p_setup.cpp
@@ -3709,6 +3709,9 @@ void P_SetupLevel(const char *lumpname, int position, bool newGame)
 		node.len = (float)g_sqrt(fdx * fdx + fdy * fdy);
 	}
 
+	// CreateVBO must be run on the plain 3D floor data.
+	P_ClearDynamic3DFloorData();
+
 	// This must be done BEFORE the PolyObj Spawn!!!
 	Renderer->PreprocessLevel();
 
