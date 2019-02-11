9f0eb35b28614d52ed4d8733b59952cecdf47d6d
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-02-11 16:26:11 +0200

    - added compatibility flag for buggy CheckSwitchRange behavior
    https://forum.zdoom.org/viewtopic.php?t=63008
    https://forum.zdoom.org/viewtopic.php?t=63650

--- src/p_switch.cpp
+++ src/p_switch.cpp
@@ -202,7 +202,9 @@ bool P_CheckSwitchRange(AActor *user, line_t *line, int sideno, const DVector3 *
 			}
 		}
 
-		return (user->Top() > open.top);
+		return (i_compatflags2 & COMPATF2_CHECKSWITCHRANGE)
+			? (user->Top() >= open.top)
+			: (user->Top() > open.top);
 	}
 	else if ((TexMan.FindSwitch(side->GetTexture(side_t::bottom))) != NULL)
 	{
@@ -224,7 +226,9 @@ bool P_CheckSwitchRange(AActor *user, line_t *line, int sideno, const DVector3 *
 			}
 		}
 
-		return (user->Z() < open.bottom);
+		return (i_compatflags2 & COMPATF2_CHECKSWITCHRANGE)
+			? (user->Z() <= open.bottom)
+			: (user->Z() < open.bottom);
 	}
 	else if ((flags & ML_3DMIDTEX) || (TexMan.FindSwitch(side->GetTexture(side_t::mid))) != NULL)
 	{
@@ -237,7 +241,9 @@ bool P_CheckSwitchRange(AActor *user, line_t *line, int sideno, const DVector3 *
 	else
 	{
 		// no switch found. Check whether the player can touch either top or bottom texture
-		return (user->Top() > open.top) || (user->isBelow(open.bottom));
+		return (i_compatflags2 & COMPATF2_CHECKSWITCHRANGE)
+			? ( (user->Top() >= open.top) || (user->Z() <= open.bottom) )
+			: ( (user->Top() > open.top)  || user->isBelow(open.bottom) );
 	}
 }
 
