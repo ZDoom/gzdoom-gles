60e1bb604e0b46cd1cc1b73140b14d20caf634d1
Christoph Oelckers <coelckers@users.noreply.github.com>
2019-02-26 00:07:30 +0100

    - removed unused gl_spritebrightfog CVAR.
    # Conflicts:
    #	src/gl/system/gl_cvars.h
    #	src/gl/system/gl_menu.cpp

--- src/gl/scene/gl_sprite.cpp
+++ src/gl/scene/gl_sprite.cpp
@@ -61,7 +61,6 @@
 #include "gl/renderer/gl_quaddrawer.h"
 
 CVAR(Bool, gl_usecolorblending, true, CVAR_ARCHIVE|CVAR_GLOBALCONFIG)
-CVAR(Bool, gl_spritebrightfog, false, CVAR_ARCHIVE|CVAR_GLOBALCONFIG);
 CVAR(Bool, gl_sprite_blend, false, CVAR_ARCHIVE|CVAR_GLOBALCONFIG);
 CVAR(Int, gl_spriteclip, 1, CVAR_ARCHIVE)
 CVAR(Float, gl_sclipthreshold, 10.0, CVAR_ARCHIVE)
