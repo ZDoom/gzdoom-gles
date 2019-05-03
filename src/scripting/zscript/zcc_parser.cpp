819b9f972d9fd1a12426fd716ef6ccf7f41b236e
drfrag <drfrag666@hotmail.com>
2019-05-03 19:09:17 +0200

    - Added "-zsvoverride" command line switch to override mod ZScript version requirement.

--- src/scripting/zscript/zcc_parser.cpp
+++ src/scripting/zscript/zcc_parser.cpp
@@ -403,7 +403,7 @@ static void DoParse(int lumpnum)
 			{
 				sc.ScriptError("Bad version directive");
 			}
-			if (state.ParseVersion > MakeVersion(VER_MAJOR, VER_MINOR, VER_REVISION))
+			if (state.ParseVersion > MakeVersion(VER_MAJOR, VER_MINOR, VER_REVISION) && !Args->CheckParm("-zsvoverride"))
 			{
 				sc.ScriptError("Version mismatch. %d.%d.%d expected but only %d.%d.%d supported", state.ParseVersion.major, state.ParseVersion.minor, state.ParseVersion.revision, VER_MAJOR, VER_MINOR, VER_REVISION);
 			}
