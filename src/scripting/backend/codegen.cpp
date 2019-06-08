b3f18e5e4751db0c48144e08ce8f1e98a0d9cbe9
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-06-08 11:12:34 +0300

    - report implicitly initialized variable in code generator once
    Vectors are no longer reported several times
    Previously, warnings were issued per every register

--- src/scripting/backend/codegen.cpp
+++ src/scripting/backend/codegen.cpp
@@ -11355,6 +11355,9 @@ ExpEmit FxLocalVariableDeclaration::Emit(VMFunctionBuilder *build)
 					auto& registers = build->Registers[regType];
 					RegNum = registers.Get(RegCount);
 
+					// Check for reused registers and clean them if needed
+					bool useDirtyRegisters = false;
+
 					for (int reg = RegNum, end = RegNum + RegCount; reg < end; ++reg)
 					{
 						if (!registers.IsDirty(reg))
@@ -11362,7 +11365,7 @@ ExpEmit FxLocalVariableDeclaration::Emit(VMFunctionBuilder *build)
 							continue;
 						}
 
-						ScriptPosition.Message(MSG_DEBUGMSG, "Implicit initialization of variable %s\n", Name.GetChars());
+						useDirtyRegisters = true;
 
 						switch (regType)
 						{
@@ -11386,6 +11389,11 @@ ExpEmit FxLocalVariableDeclaration::Emit(VMFunctionBuilder *build)
 							assert(false);
 							break;
 						}
+					}
+
+					if (useDirtyRegisters)
+					{
+						ScriptPosition.Message(MSG_DEBUGMSG, "Implicit initialization of variable %s", Name.GetChars());
 					}
 				}
 				else
