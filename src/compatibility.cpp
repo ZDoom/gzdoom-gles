dca15d56c889f4a7bae2ad51d808d66161c51d41
alexey.lysiuk <alexey.lysiuk@gmail.com>
2019-10-27 16:36:59 +0200

    - extended level post-processing with thing IDs
    # Conflicts:
    #	src/maploader/maploader.cpp

--- src/compatibility.cpp
+++ src/compatibility.cpp
@@ -669,6 +669,29 @@ DEFINE_ACTION_FUNCTION(DLevelPostProcessor, SetThingArgument)
 	return 0;
 }
 
+DEFINE_ACTION_FUNCTION(DLevelPostProcessor, GetThingID)
+{
+	PARAM_SELF_PROLOGUE(DLevelPostProcessor);
+	PARAM_UINT(thing);
+
+	const int id = thing < MapThingsConverted.Size()
+		? MapThingsConverted[thing].thingid : 0;
+	ACTION_RETURN_INT(id);
+}
+
+DEFINE_ACTION_FUNCTION(DLevelPostProcessor, SetThingID)
+{
+	PARAM_SELF_PROLOGUE(DLevelPostProcessor);
+	PARAM_UINT(thing);
+	PARAM_INT(id);
+
+	if (thing < MapThingsConverted.Size())
+	{
+		MapThingsConverted[thing].thingid = id;
+	}
+	return 0;
+}
+
 DEFINE_ACTION_FUNCTION(DLevelPostProcessor, SetVertex)
 {
 	PARAM_SELF_PROLOGUE(DLevelPostProcessor);
