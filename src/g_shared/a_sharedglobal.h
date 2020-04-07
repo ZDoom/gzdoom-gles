b658d4c225fd4b3fbc48430b41dff045efafdefb
Mekboss <speedypablogonsales@gmail.com>
2020-04-07 23:11:49 +0300

    Fix MSVS compile bug and add offset parameter for SprayDecal
    # Conflicts:
    #	src/g_shared/a_sharedglobal.h

--- src/g_shared/a_sharedglobal.h
+++ src/g_shared/a_sharedglobal.h
@@ -11,7 +11,7 @@ struct F3DFloor;
 class DBaseDecal;
 
 class DBaseDecal *ShootDecal(const FDecalTemplate *tpl, AActor *basisactor, sector_t *sec, double x, double y, double z, DAngle angle, double tracedist, bool permanent);
-void SprayDecal(AActor *shooter, const char *name,double distance = 172., DVector3 Dir = (0, 0, 0) );
+void SprayDecal(AActor *shooter, const char *name,double distance = 172., DVector3 offset = DVector3(0., 0., 0.), DVector3 direction = DVector3(0., 0., 0.) );
 
 class DBaseDecal : public DThinker
 {
