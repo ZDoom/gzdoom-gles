8ec5a4616bc9bc0c51be7de2b5bc4f4767770ead
Christoph Oelckers <coelckers@users.noreply.github.com>
2020-06-04 01:15:44 +0200

    - fixed: vertically mirrored textures should not be subjected to empty space optimizations because the algorithm cannot deal with the inverted case.
    # Conflicts:
    #	src/gl/scene/gl_wall.h

--- src/gl/scene/gl_walls.cpp
+++ src/gl/scene/gl_walls.cpp
@@ -957,6 +957,7 @@ void GLWall::DoMidTexture(seg_t * seg, bool drawfogboundary,
 	{
 		tci.mRenderHeight = -tci.mRenderHeight;
 		tci.mScale.Y = -tci.mScale.Y;
+		flags |= GLWF_NOSLICE;
 	}
 	SetWallCoordinates(seg, &tci, texturetop, topleft, topright, bottomleft, bottomright, t_ofs);
 
@@ -1017,7 +1018,7 @@ void GLWall::DoMidTexture(seg_t * seg, bool drawfogboundary,
 		FloatRect *splitrect;
 		int v = gltexture->GetAreas(&splitrect);
 		if (seg->frontsector == seg->backsector) flags |= GLWF_NOSPLIT;	// we don't need to do vertex splits if a line has both sides in the same sector
-		if (v>0 && !drawfogboundary && !(seg->linedef->flags&ML_WRAP_MIDTEX))
+		if (v>0 && !drawfogboundary && !(seg->linedef->flags & ML_WRAP_MIDTEX) && !(flags & GLWF_NOSLICE))
 		{
 			// split the poly!
 			int i,t=0;
