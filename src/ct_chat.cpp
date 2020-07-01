0a210ed7f0dfea94d0b0c24fb3bd661a94b57fa8
drfrag <drfrag666@hotmail.com>
2020-07-02 00:12:24 +0200

    - Disable chat for single player, controlled by the chat_self CVAR.

--- src/ct_chat.cpp
+++ src/ct_chat.cpp
@@ -58,6 +58,8 @@ EXTERN_CVAR (Bool, sb_cooperative_enable)
 EXTERN_CVAR (Bool, sb_deathmatch_enable)
 EXTERN_CVAR (Bool, sb_teamdeathmatch_enable)
 
+CVAR (Bool, chat_self, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
+
 int active_con_scaletext();
 
 // Public data
@@ -507,7 +509,7 @@ static bool DoSubstitution (FString &out, const char *in)
 
 CCMD (messagemode)
 {
-	if (menuactive == MENU_Off)
+	if (menuactive == MENU_Off && (multiplayer || chat_self))
 	{
 		chatmodeon = 1;
 		C_HideConsole ();
@@ -529,7 +531,7 @@ CCMD (say)
 
 CCMD (messagemode2)
 {
-	if (menuactive == MENU_Off)
+	if (menuactive == MENU_Off && (multiplayer || chat_self))
 	{
 		chatmodeon = 2;
 		C_HideConsole ();
