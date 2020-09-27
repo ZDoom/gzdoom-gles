b0b9aa71dc671b1e9f89da4ac1cf04cd8bf69aa6
drfrag <drfrag666@hotmail.com>
2020-09-27 20:10:25 +0200

    - ZScript version bump.

--- src/version.h
+++ src/version.h
@@ -49,8 +49,8 @@ const char *GetVersionString();
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
-#define VER_MINOR 4
-#define VER_REVISION 2
+#define VER_MINOR 5
+#define VER_REVISION 0
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
