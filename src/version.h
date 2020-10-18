b752e0ad9bfe00bfdce06b92c12457b084de857e
drfrag <drfrag666@hotmail.com>
2020-10-18 21:28:12 +0200

    - Version 3.87a.

--- src/version.h
+++ src/version.h
@@ -41,11 +41,11 @@ const char *GetVersionString();
 
 /** Lots of different version numbers **/
 
-#define VERSIONSTR "3.87"
+#define VERSIONSTR "3.87a"
 
 // The version as seen in the Windows resource
-#define RC_FILEVERSION 3,87,0
-#define RC_PRODUCTVERSION 3,87,0
+#define RC_FILEVERSION 3,87,1
+#define RC_PRODUCTVERSION 3,87,1
 #define RC_PRODUCTVERSION2 VERSIONSTR
 // These are for content versioning.
 #define VER_MAJOR 4
