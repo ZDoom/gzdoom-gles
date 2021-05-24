e97539c3fe7c7e44dc78c3369d2ded02c171879c
Emile Belanger <emile.belanger@gmail.com>
2021-05-24 21:35:47 +0100

    Delete some unused code

--- src/common/rendering/gles/gles_shader.cpp
+++ src/common/rendering/gles/gles_shader.cpp
@@ -394,25 +394,9 @@ bool FShader::Load(const char * name, const char * vert_prog_lump_, const char *
 
 	bool lightbuffertype = screen->mLights->GetBufferType();
 	unsigned int lightbuffersize = screen->mLights->GetBlockSize();
-	if (!lightbuffertype)
-	{
-		vp_comb.Format("#version 100\n#define NUM_UBO_LIGHTS %d\n#define NO_CLIPDISTANCE_SUPPORT\n", lightbuffersize);
-	}
-	/*
-	else
-	{
-		// This differentiation is for Intel which do not seem to expose the full extension, even if marked as required.
-		if (gles.glslversion < 4.3f)
-			vp_comb = "#version 400 core\n#extension GL_ARB_shader_storage_buffer_object : require\n#define SHADER_STORAGE_LIGHTS\n";
-		else
-			vp_comb = "#version 430 core\n#define SHADER_STORAGE_LIGHTS\n";
-	}
 
-	if (gl.flags & RFL_SHADER_STORAGE_BUFFER)
-	{
-		vp_comb << "#define SUPPORTS_SHADOWMAPS\n";
-	}
-	*/
+	vp_comb.Format("#version 100\n#define NUM_UBO_LIGHTS %d\n#define NO_CLIPDISTANCE_SUPPORT\n", lightbuffersize);
+
 	FString fp_comb = vp_comb;
 	vp_comb << defines << i_data.GetChars();
 	fp_comb << "$placeholder$\n" << defines << i_data.GetChars();
